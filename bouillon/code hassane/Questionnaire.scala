65ead59e36bf0479b97123d957948777e2458815
DIABY Hassane <hasdiaby@gmail.com>
2017-12-05 12:37:38 +0100

    Suppression code hassane

--- bouillon/code hassane/Questionnaire.scala
+++
@@ -1,8 +0,0 @@
-
-class Questionnaire(_id:Int,_type:Int){
-	var id_q = _id;
-	var type_q = _type
-	var questions : List[Question] = Nil
-	def addQuestion(q:Question) = questions = questions :+ q
-}
-

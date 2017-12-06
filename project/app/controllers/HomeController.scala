19a3fef1c8e8c84797bed43456f5b9064c2750f5
CactusTribe <lefrancjoaquim@gmail.com>
2017-12-06 14:44:36 +0100

    BD test ok

--- project/app/controllers/HomeController.scala
+++ project/app/controllers/HomeController.scala
@@ -2,13 +2,14 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import play.api.db._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents, db: Database) extends AbstractController(cc) {
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -16,12 +17,23 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
+
+  db.withConnection { conn =>
+     val stmt = conn.createStatement
+     val rs = stmt.execute("INSERT INTO users (pseudo, password) VALUES ('TEST', 'PASS');")
+   }
+
+  var resultat = ""
+  db.withConnection { conn =>
+    val stmt = conn.createStatement
+    val rs = stmt.executeQuery("SELECT * FROM users;")
+
+    while (rs.next()) {
+      resultat += rs.getString("pseudo")
+    }
+  }
+
   def index = Action {
-    Ok(views.html.index("EVALUATOR"))
+    Ok(views.html.index(resultat))
   }
-
-  DB.withConnection { conn =>
-  // do whatever you need with the connection
-  }
-
 }
--- project/conf/evolutions/default/1.sql
+++ project/conf/evolutions/default/1.sql
@@ -4,11 +4,10 @@
 
 CREATE TABLE users (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    first_name varchar(255) NOT NULL,
-    last_name varchar(255) NOT NULL,
+    pseudo varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
     PRIMARY KEY (id),
-    UNIQUE KEY (email)
+    UNIQUE KEY (pseudo)
 );
 
 # --- !Downs

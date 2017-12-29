72aa8a87f4765d3a12adde2890f742e2bf9f4274
Xiang LI <xiangfr007@gmail.com>
2017-12-29 11:59:11 +0100

    add question type qcm fill image

--- project/app/controllers/QuestionController.scala
+++ project/app/controllers/QuestionController.scala
@@ -3,9 +3,10 @@ package controllers
 import javax.inject._
 import play.api.mvc._
 import play.api.db._
-
+import play.api.data._
+import play.api.data.Forms._
 import models._
-
+import java.io.File
 /**
  * This controller creates an `Action` to handle HTTP requests to "questions"
  */
@@ -30,6 +31,71 @@ class QuestionController @Inject()(cc: ControllerComponents, db: Database) exten
 
   def delete(id:Int) = Action{
     DBQuestion.delete(id)
+    Redirect(routes.QuestionController.index())
+  }
+
+  def addpage = Action { 
+    DBQuestion.applySeeder()
+    var nowid = DBQuestion.dbmaps.size
+    Ok(views.html.questionaddpage(nowid))
+  }
+
+  def addqcm = Action {implicit request =>
+
+    val addForm = Form(
+      tuple(
+        "qid" -> text,
+        "title" -> text,
+        "op1" -> text,
+        "op2" -> text,
+        "op3" -> text,
+        "op4" -> text,
+        "op5" -> text,
+        "op6" -> text,
+        "answer" -> text,
+      )
+    )
+    val (qid,title,op1,op2,op3,op4,op5,op6,answer) = addForm.bindFromRequest.get
+    val addquestion = new QCMQuestion(qid.toInt,title,answer,List("A."+op1,"B."+op2,"C."+op3,"D."+op4,"E."+op5,"F."+op6))
+    DBQuestion.add(qid.toInt,addquestion)
+    Redirect(routes.QuestionController.index())
+  }
+
+  def addfill = Action {implicit request =>
+
+    val addForm = Form(
+      tuple(
+        "qid" -> text,
+        "title" -> text,
+        "answer" -> text,
+      )
+    )
+    val (qid,title,answer) = addForm.bindFromRequest.get
+    val addquestion = new FillQuestion(qid.toInt,title,answer)
+    DBQuestion.add(qid.toInt,addquestion)
+    Redirect(routes.QuestionController.index())
+  }
+
+  def addimage = Action(parse.multipartFormData) {implicit request =>
+
+    val addForm = Form(
+      tuple(
+        "qid" -> text,
+        "title" -> text,
+        "answer" -> text,
+      )
+    )
+    val (qid,title,answer) = addForm.bindFromRequest.get
+
+    request.body.file("pic").map { picture =>
+      var name = "public/images/pic"+answer+qid+".png"
+      picture.ref.moveTo(new File(name))
+    }.getOrElse {
+      Redirect(routes.QuestionController.index())
+    }
+
+    val addquestion = new ImageFillQuestion(qid.toInt,title,answer,"pic"+answer+qid)
+    DBQuestion.add(qid.toInt,addquestion)
     Redirect(routes.QuestionController.index())
   }
 }

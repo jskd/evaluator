41e77d70b0313b3ec70069849f60235cd2231108
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-02 17:02:04 +0100

    Ajout css custom et liste à l'acceuil

--- project/app/controllers/HomeController.scala
+++ project/app/controllers/HomeController.scala
@@ -4,6 +4,7 @@ import javax.inject._
 import play.api.mvc._
 import play.api.db._
 import scala.util.Try
+import models._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -14,14 +15,25 @@ class HomeController @Inject()(cc: ControllerComponents, db: Database) extends A
 
   def index = Action { request =>
 
+    DBQuestionnaire.applySeeder()
+    DBQuestion.applySeeder()
+    var qlist = DBQuestionnaire.getAll()
+    for(ql <- qlist){
+        ql.questions = Nil
+        for(id <- ql.qid){
+            if(DBQuestion.has(id))
+                ql.addQuestion(DBQuestion.getById(id))
+        }
+    }
+
     request.session.get("connected").map { user =>
       request.session.get("admin").map { admin =>
-        Ok(views.html.index(user, Try(admin.toBoolean).getOrElse(false)))
+        Ok(views.html.index(qlist, user, Try(admin.toBoolean).getOrElse(false)))
       }.getOrElse {
-        Ok(views.html.index())
+        Ok(views.html.index(qlist))
       }
     }.getOrElse {
-      Ok(views.html.index())
+      Ok(views.html.index(qlist))
       //Unauthorized("Oops, you are not connected")
     }
 

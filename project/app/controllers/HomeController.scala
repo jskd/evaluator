d43639251714feca82a8aed51dd97f65bdeb92e4
Jérôme Skoda <contact@jeromeskoda.fr>
2018-01-08 22:07:53 +0100

    Change to redirection

--- project/app/controllers/HomeController.scala
+++ project/app/controllers/HomeController.scala
@@ -44,12 +44,11 @@ class HomeController @Inject()(cc: ControllerComponents, db: Database) extends A
   }
 
   def add(qid:Int) = Action {implicit request =>
-    var qlist = DBCours.getAll()
-    Ok(views.html.index(qlist))
+    Redirect(routes.HomeController.index())
   }
 
   def delete(qid:Int) = Action{
-    var qlist = DBCours.getAll()
-    Ok(views.html.index(qlist))
+
+    Redirect(routes.HomeController.index())
   }
 }
